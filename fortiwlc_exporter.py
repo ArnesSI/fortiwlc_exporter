539c90693d82c7e2acf1208454d8443b2f8bb881
Luka Vadnjal <luka.vadnjal@guest.arnes.si>
2018-08-30 08:12:57 +0200

    added fortiwlc_up metric

--- fortiwlc_exporter.py
+++ fortiwlc_exporter.py
@@ -30,52 +30,22 @@ wlcarray = sys.argv[1]
 class FortiwlcCollector(object):
     def collect(self):
         fortiwlc_clients_by_ap_count = GaugeMetricFamily('fortiwlc_clients_by_ap_count','help',labels=['ap_name','campus_name','profile_name','model','wlc','status','state'])
+        fortiwlc_up = GaugeMetricFamily('fortiwlc_up','help')
 
-        for ap_name, ap_data in main(ssidapi,wlcarray)['ap'].items():
-            fortiwlc_clients_by_ap_count.add_metric([ap_name,ap_data['campus_name'],ap_data['profile_name'],ap_data['model'],ap_data['wlc'],ap_data['status'],ap_data['state']], ap_data['client_count'])
+        try:
+            wlc_data = main(ssidapi,wlcarray)
+
+            for ap_name, ap_data in wlc_data['ap'].items():
+                fortiwlc_clients_by_ap_count.add_metric([ap_name,ap_data['campus_name'],ap_data['profile_name'],ap_data['model'],ap_data['wlc'],ap_data['status'],ap_data['state']], ap_data['client_count'])
+
+        except:
+            fortiwlc_up.add_metric([],0)
+
+        else:
+            fortiwlc_up.add_metric([],1)
 
         yield fortiwlc_clients_by_ap_count
-
-        '''client_count = GaugeMetricFamily('ap_client_count','help',labels=['ap_name'])
-        ssid_count = GaugeMetricFamily('ssid_count','help',labels=['ap_name'])'''
-
-        '''for campus_name, campus_data in main(ssidapi,production)['campus'].items():
-            #print(campus_data)
-            ap_count.add_metric([campus_name+' total'], campus_data['ap_count']['total'])
-            ap_count.add_metric([campus_name+' reeboot_last_day'], campus_data['ap_count']['reboot_last_day'])
-            ap_count.add_metric([campus_name+' connected'], campus_data['ap_count']['status']['connected'])
-            ap_count.add_metric([campus_name+' disconnected'], campus_data['ap_count']['status']['disconnected'])
-            ap_count.add_metric([campus_name+' authorized'], campus_data['ap_count']['status']['authorized'])
-            ap_count.add_metric([campus_name+' discovered'], campus_data['ap_count']['status']['discovered'])
-            ap_count.add_metric([campus_name+' model FAP221E'], campus_data['ap_count']['per_model'].get('FAP221E'))
-
-
-        for wlc_name, wlc_data in main(ssidapi,production)['wlc'].items():
-            ap_count.labels(wlc_name=wlc_name, wlc_label='wlc_slug').set(wlc_data['ap_count']['total'])
-            ap_count.add_metric([wlc_name+' reboot_last_day'], wlc_data['ap_count']['reboot_last_day'])
-            ap_count.add_metric([wlc_name+' connected'], wlc_data['ap_count']['status']['connected'])
-            ap_count.add_metric([wlc_name+' disconnected'], wlc_data['ap_count']['status']['disconnected'])
-            ap_count.add_metric([wlc_name+' authorized'], wlc_data['ap_count']['status']['authorized'])
-            ap_count.add_metric([wlc_name+' discovered'], wlc_data['ap_count']['status']['discovered'])
-
-'''
-        '''for ap_name,ap_data in main(ssidapi,production)['ap'].items():
-            ap_client_count.add_metric([ap_name], ap_data['client_count'])
-            ssid_count.add_metric([ap_name], ap_data['ssid_count'])
-            radio1_count.add_metric([ap_name], ap_data['per_radio']['1'])
-            radio2_count.add_metric([ap_name], ap_data['per_radio']['2'])
-
-        yield ap_client_count
-        yield ssid_count
-        yield radio1_count
-        yield radio2_count
-
-
-        campus_client_count = GaugeMetricFamily('campus_client_count','help',labels=['campus_name'])
-        for campus_name,campus_data in main(ssidapi,production)['campus'].items():
-            campus_client_count.add_metric([campus_name], campus_data['client_count'])
-
-        yield campus_client_count'''
+        yield fortiwlc_up
 
 
 if __name__ == "__main__":
