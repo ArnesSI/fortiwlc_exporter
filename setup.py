6c5cc1803c63dca98196875db6fbd78594959bdd
Matej Vadnjal <matej.vadnjal@arnes.si>
2018-10-14 17:49:29 +0200

    add testing utils

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://git.arnes.si/monitoring/fortiwlc_exporter",
     packages=setuptools.find_packages(),
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
