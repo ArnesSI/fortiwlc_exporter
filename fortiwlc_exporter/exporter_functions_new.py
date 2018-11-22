6ccefc18eccb57f366531ed500768bc665e62c06
Matej Vadnjal <matej.vadnjal@arnes.si>
2018-11-22 13:04:09 +0100

    cleanup unused code

--- fortiwlc_exporter/exporter_functions_new.py
+++ fortiwlc_exporter/exporter_functions_new.py
@@ -1,41 +1,3 @@
-import requests
-import json
-import sys
-from pprint import pprint
-
-from .fortiwlc import FortiWLC
-
-
-#ap_profile dictionaries
-ap_profile_dict = {}
-wlcprofiles = {}
-data = None
-datassid = None
-#restructure the whole script
-totalcamps = 0
-totalaps = 0
-totalwlccamps = 0
-totalwlcaps = 0
-
-maindict = {
-    'ap': {}
-}
-
-
-
-
-def init():
-    global ap_profile_dict, wlcprofiles, data, datassid, maindict
-    ap_profile_dict = {}
-    wlcprofiles = {}
-    data = None
-    datassid = None
-
-    maindict = {
-        'ap': {}
-    }
-
-
 def client_count(path,ap,perradio):
     clients = ap['clients']
     path['client_count'] += clients
@@ -89,24 +51,3 @@ def parse_ap_data(ap_data, wlc_name):
     }
     client_count(ap, ap_data, True)
     return ap
-
-
-def ap_read_trough(wlc_name):
-    for ap in data:
-        break
-        #ap_write(ap,wlc_name)
-
-
-def main(wlc_group):
-    global data, datassid
-    init()
-
-    for wlc_name, api_key in wlc_group:
-        data = FortiWLC(wlc_name, api_key).get_managed_ap()
-        ap_read_trough(wlc_name)
-    return maindict
-
-
-#if __name__ == "__main__":
-#    wlcarray = sys.argv[1]
-#    print(json.dumps(main(ssidapi,wlcarray), indent = 4, sort_keys = False))
