f7b5440ebefa76ce2235d93f6110a17ca21bfee7
Matej Vadnjal <matej.vadnjal@arnes.si>
2018-11-22 09:25:55 +0100

    add: FortiWLC class for fetching data from WLC

---
+++ fortiwlc_exporter/fortiwlc.py
@@ -0,0 +1,27 @@
+import requests
+
+
+class FortiWLC:
+    """ Class that collects WIFI data from FortiNET API """
+
+    MANAGED_AP_URL = 'https://{name}/api/v2/monitor/wifi/managed_ap/select/?vdom=root&access_token={api_key}'  # noqa
+    VAP_GROUP_URL = 'https://{name}/api/v2/cmdb/wireless-controller/vap-group/?vdom=root&access_token={api_key}'  # noqa
+
+    def __init__(self, name, api_key):
+        self.name = name
+        self.api_key = api_key
+
+    def _get(self, url):
+        resp = requests.get(url)
+        resp.raise_for_status()
+        return resp.json()['results']
+
+    def get_managed_ap(self):
+        """ Returns info about APs (access points) """
+        url = self.MANAGED_AP_URL.format(name=self.name, api_key=self.api_key)
+        return self._get(url)
+
+    def get_vap_group(self):
+        """ Returns info about configured VAPs (SSIDs) """
+        url = self.VAP_GROUP_URL.format(name=self.name, api_key=self.api_key)
+        return self._get(url)
