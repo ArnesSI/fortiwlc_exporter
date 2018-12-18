e42d01e413c2b1f184977662af03ddcd1e746f36
Matej Vadnjal <matej.vadnjal@arnes.si>
2018-12-18 12:03:24 +0100

    bugfix in dev-version script

--- dev-version.sh
+++ dev-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if we are on a tag (based on GitLab CI var or git describe)
-if [ -z ${CI_TAG_REF+x} ] && ! git describe --exact-match 2>/dev/null; then
+if [ -z ${CI_COMMIT_TAG+x} ] && ! git describe --exact-match --tags 2>/dev/null; then
     # really not on a tag - append rev info to version
     VER_APPEND=$(git describe --tags | cut -d'-' -f 2- | sed s/-/+/g)
     if [ -z ${VER_APPEND} ]; then exit 1; fi
