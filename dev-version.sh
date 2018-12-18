396db4cab32248a8a981e570d0011698dbf53067
Matej Vadnjal <matej.vadnjal@arnes.si>
2018-12-18 11:37:25 +0100

    ci fixes

--- dev-version.sh
+++ dev-version.sh
@@ -4,5 +4,6 @@
 if [ -z ${CI_TAG_REF+x} ] && ! git describe --exact-match 2>/dev/null; then
     # really not on a tag - append rev info to version
     VER_APPEND=$(git describe --tags | cut -d'-' -f 2-)
+    if [ -z ${VER_APPEND} ]; then exit 1; done
     bumpversion --no-commit --no-tag --allow-dirty --list --serialize "{major}.{minor}.{patch}-${VER_APPEND}" patch
 fi
