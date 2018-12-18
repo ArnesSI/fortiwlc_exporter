1de92ca2be8817c64b16beb18af354c46b643f8c
Matej Vadnjal <matej.vadnjal@arnes.si>
2018-12-18 12:11:02 +0100

    Bump version: 1.5.4 → 1.5.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fortiwlc_exporter",
-    version="1.5.4",
+    version="1.5.5",
     author="Luka Vadnjal",
     author_email="luka@vadnjal.net",
     description="FortiWLC Prometheus exporter",
