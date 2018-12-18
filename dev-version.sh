c6edc537ea3cab6797bfa27d6eb3a70bcb7a3009
Matej Vadnjal <matej.vadnjal@arnes.si>
2018-12-18 11:44:11 +0100

    rpm don't like - in version

--- dev-version.sh
+++ dev-version.sh
@@ -3,7 +3,7 @@
 # Check if we are on a tag (based on GitLab CI var or git describe)
 if [ -z ${CI_TAG_REF+x} ] && ! git describe --exact-match 2>/dev/null; then
     # really not on a tag - append rev info to version
-    VER_APPEND=$(git describe --tags | cut -d'-' -f 2-)
+    VER_APPEND=$(git describe --tags | cut -d'-' -f 2- | sed s/-/+/g)
     if [ -z ${VER_APPEND} ]; then exit 1; fi
-    bumpversion --no-commit --no-tag --allow-dirty --list --serialize "{major}.{minor}.{patch}-${VER_APPEND}" patch
+    bumpversion --no-commit --no-tag --allow-dirty --list --serialize "{major}.{minor}.{patch}.dev${VER_APPEND}" patch
 fi
